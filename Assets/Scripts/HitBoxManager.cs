a2727358916c50832b5655161842c42ab5c1c3b9
Matthew Zane <matthewdzane@gmail.com>
2019-05-04 01:55:03 -0700

    First commit

---
+++ Assets/Scripts/HitBoxManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCubeManager : MonoBehaviour
+{
+    private int pointValue;
+
+    public GameObject preCube;
+    public GameObject postCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public bool isHit()
+    {
+        //if the precube was hit already
+        if (preCube == null)
+        {
+            // first set postcube.precube to null
+            postCube.GetComponent<HitCubeManager>().preCube = null;
+
+            // then destroy this cube
+            Destroy(this);
+        }
+        return false;
+    }
+
+
+}
---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public GameObject hitCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawnSingle()
+    {
+
+    }
+
+    public void spawnCombo(GameObject preCombo)
+    {
+        Instantiate(hitCube, this.transform);
+    }
+}
